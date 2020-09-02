4ab14c490fed0b714fe14b819466629c92de2bb3
amichalo421 <andre.michalowski@gmail.com>
2020-09-02 13:45:27 -0400

    Step 2: useDarkMode (toggle)

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -2,5 +2,14 @@ import useLocalStorage from './useLocalStorage';
 import { useEffect } from 'react';
 
 export const useDarkMode = (key, initialValue) => {
-  const [exValue, setExValue] = useLocalStorage('darkmode')
+  const [newValue, setNewValue] = useLocalStorage('darkmode')
+  useEffect(() => {
+    if(newValue === true){
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+  }, [newValue]);
+
+  return[newValue, setNewValue];
 }
