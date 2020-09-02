e9b8b707428d326260d92832b20d4135b7187947
amichalo421 <andre.michalowski@gmail.com>
2020-09-02 12:57:47 -0400

    Add 'storedValue' to 'useLocalStorage'

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,1 +1,10 @@
-const [value, setValue] = useLocalStorage('key', 'initialValue')
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  return [storedValue];
+};
